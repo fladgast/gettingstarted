df5ee2520a1f8ab14e4293b58fb6e1e4e2c7169f
Emil <Emil.Johansson@awacademy.training>
2022-11-24 13:23:29 +0100

    Task 7 complete

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent { }
+export class TopBarComponent {
+
+  constructor(
+    private cart: CartService
+  ) {}
+
+  getSizeOfCart() {
+    return this.cart.getItems().length
+  }
+ }
